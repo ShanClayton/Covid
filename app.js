141d75e030190cce43c845387dfed89b7725b1c1
Shanaun <sclayton1183@gmail.com>
2020-04-30 02:39:58 -0700

    fixed js error

---
+++ app.js
@@ -0,0 +1,15 @@
+fetch('https://corona.lmao.ninja/v2/countries/USA')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        document.getElementById("country").innerHTML = data.country;
+        document.getElementById("active").innerHTML = data.active.toLocaleString();
+        document.getElementById("cases").innerHTML = data.cases.toLocaleString();
+        document.getElementById("critical").innerHTML = data.critical.toLocaleString();
+        document.getElementById("death").innerHTML = data.deaths.toLocaleString();
+        document.getElementById("recovery").innerHTML = data.recovered.toLocaleString();
+        document.getElementById("test").innerHTML = data.tests.toLocaleString();
+        document.getElementById("race").innerHTML = data.tests.toLocaleString();
+    });
